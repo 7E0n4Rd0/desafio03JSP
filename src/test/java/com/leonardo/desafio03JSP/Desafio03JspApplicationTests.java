e9475c9e582bcbdafdceb639592f3707ee4ac67e
leonArch <leomeireles66@gmail.com>
2024-12-24 07:26:02 -0300

    Project Created

---
+++ src/test/java/com/leonardo/desafio03JSP/Desafio03JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.desafio03JSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio03JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
